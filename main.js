60fab70122dededc9746299d7fa41acb7c219e4b
UnlikeDogVirtual3532 <kobe@roosemeyers.com>
2024-02-28 18:36:49 +0100

    modified:   credits.html 	modified:   index.html 	new file:   main.js 	new file:   resources/svg/logic-gate-xor.svg 	modified:   style.css

---
+++ main.js
@@ -0,0 +1,7 @@
+const wrapper = document.querySelector(".Board")
+const svgScene = wrapper.querySelector("svg")
+const content = wrapper.querySelector(".content")
+
+const sources = []
+let currentLine = null
+let drag = false
